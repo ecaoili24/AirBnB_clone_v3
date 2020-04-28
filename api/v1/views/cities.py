84a3036a8e4c8097a747e90029aa276ab31bdd92
ecaoili24 <erika.caoili@gmail.com>
2020-04-28 15:12:20 +0000

    Initial commit of task 8

---
+++ api/v1/views/cities.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application"""
+
+import os
+from flask import Flask, jsonify, abort, request
+from models import storage
+from api.v1.views import app_views
+from models.state import State
+from models.state import City
+from models.city import City
+
+app = Flask(__name__)
+
+
+@app_views.route('/states/<states_id>/cities', methods=['GET'],
+                 strict_slashes=False)
+def get_cities(state_id=None):
+    """List retrieval of all City objects of a State"""
+    states = storage.all('State')
+    state = states.get('State' + "." + state_id)
+    if state is None:
+        abort(404)  # state_id is not linked to any State object, raise 404 err
+    cityLIST = []
+    cities = storage.all('City')
+    for city in cities.values():
+        if city.state_id == state_id:
+            cityLIST.append(city.to_dict())  # serializes an obj to valid JSON
+    return jsonify(cityLIST), 200  # return empty list with status code 200
+
+
+@app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
+def city_retrieval(state_id=None):
+    """Retrieval of City objects with linked ids"""
+    city_dict = storage.all('City')
+    city = city_dict.get('City' + "." + city_id)
+    if city is None:  # if state_id is not linked to any State obj
+        abort(404)
+    else:
+        return jsonify(city.to_dict()), 200
+
+
+@app_views.route('/cities/<city_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_city(city_id=None):
+    """Deletes a City object"""
+    object = storage.get('City', city_id)
+    if object is None:
+        abort(404)  # if the city_id is not linked to any City object
+    else:
+        storage.delete(object)
+        storage.save()
+    return jsonify({}), 200  # returns an empty dict with status code 200
+
+
+@app_views.route('/states/<state_id>/cities', method=['POST'],
+                 strict_slashes=False)
+def create_city(state_id=None):
+    """Create a City, from data provided by the request"""
+    state_dict = storage.all('State')
+    state = state_dict.get('State' + "." + state_id)
+    if state is None:
+        abort(404)
+    body = request.get_json()  # transfrom the HTTP body request to dict
+    if body is None:  # if HTTP body req is  not a valid JSON
+        return jsonify({"error": "Not a JSON"}), 400
+    if 'name' not in task:  # if dict doesn't contain the key name
+        return jsonify({"error": "Missing name"}), 400
+    object = City(name=task['name'], state_id=state_id)
+    storage.new(object)
+    storage.save()
+    return jsonify(object.to_dict()), 201  # returns new City
+
+
+@app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=Flashes)
+def update_city(city_id=None):
+    """Updating an existing City object"""
+    body = request.get_json()
+    if body is None:
+        return jsonify({"error": "Not a JSON"}), 400
+    object = storage.get('City', city_id)
+    if object is None:  # if city_id is not linked to any City object
+        abort(404)
+    ignore_keys = ['id', 'created_at', 'updated_at']  # ignore keys
+    for key, value in task.items():  # update City obj with key-val pairs
+        if key not in ignore_keys:
+            setattr(object, key, value)
+    storage.save()
+    return jsonify(object.to_dict()), 200  # return City obj
