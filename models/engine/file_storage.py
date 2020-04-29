76ca857e5defa7c3f89b4d9ee9a3642d009cc9f5
waffle52 <873@holbertonschool.com>
2020-04-29 18:22:47 +0000

    Changed to count from peer

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -79,9 +79,14 @@ class FileStorage:
                     return (val)
             return (None)
 
-    def count(self, cls=None):
-        """counts objects in storage optionally matching optional with cls"""
+     def count(self, cls=None):
+        """Get count of all objects or objects of a specific class"""
+        obj_count = 0
+        obj_list = []
         if cls is None:
-            return (len(self.all()))
+            for val in classes.values():
+                obj_count += self.__session.query(val).count()
         else:
-            return (len(self.all(cls)))
+            if cls in classes:
+                obj_count += self.__session.query(classes[cls]).count()
+        return obj_count
