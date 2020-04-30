bc1071f38c60883b7f0e06e1c533c4a0080ee378
ecaoili24 <erika.caoili@gmail.com>
2020-04-30 05:26:46 +0000

    Fixed a wrong import

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -8,7 +8,7 @@ from flask import Flask, jsonify, abort, request
 from models import storage
 from api.v1.views import app_views
 from models.state import State
-from models.state import City
+from models.city import City
 
 
 @app_views.route('/states/<state_id>/cities', methods=['GET'],
