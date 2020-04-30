0e09519f1ca7803045f6f8fa58ea3074b9ec920d
ecaoili24 <erika.caoili@gmail.com>
2020-04-30 01:15:17 +0000

    Removed a str __name__ to fix an Attribute error

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -79,7 +79,7 @@ class DBStorage:
         """returns object with key matching cls and id"""
         if cls is not None and id is not None:
             test = self.all(cls)
-            word = "{}.{}".format(cls.__name__, id)
+            word = "{}.{}".format(cls, id)
             for key, val in test.items():
                 if key == word:
                     return (val)
